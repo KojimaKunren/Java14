af4c2103a948ddbffabf695b92a4a6c06231fba3
kojimakunren <kojimakunren@gmail.com>
2023-07-20 20:01:29 +0900

    change1

---
+++ src/Hero.java
@@ -0,0 +1,15 @@
+
+public class Hero {
+	private String name;
+
+	public void setString(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.setString("aaa");
+		System.out.println(h);
+	}
+
+}
